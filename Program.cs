a39d6781df86fd63e4c9cef421c5a9f3141922d8
Azizjon Muminov <azizjon@Azizjons-MacBook-Pro.local>
2020-03-27 09:32:40 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testReview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
